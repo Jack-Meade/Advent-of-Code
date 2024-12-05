41afbe4805983a0b34c74a6f256b4dd9ced95331
Jack-Meade <jack-meade@outlook.com>
2024-12-05 15:03:53 +0000

    Refactor day2 for p2

--- 2024/day2/script.py
+++ 2024/day2/script.py
@@ -4,13 +4,29 @@ FILENAME = "example.txt" if 0 else "input.txt"
 with open(FILENAME, "r") as file:
   data = file.read().split("\n")
 
-def isLevelSafe(level):
-  if level != sorted(level) and level != sorted(level, reverse=True):
+def get_order(level):
+  if level[0] - level[1] < 0:
+    return True
+  elif level[0] - level[1] > 0:
+    return False
+  return None
+
+def is_level_safe(level):
+  ascending = get_order(level)
+  if ascending == None:
     return False
   
   for i in range(len(level) - 1):
-    diff = abs(level[i] - level[i + 1])
-    if diff == 0 or diff > 3:
+    diff = level[i] - level[i + 1]
+    if diff == 0:
+      return False
+    if diff < 0 and not ascending:
+      return False
+    if diff > 0 and ascending:
+      return False
+
+    abs_diff = abs(diff)
+    if abs_diff == 0 or abs_diff > 3:
       return False
   
   return True
@@ -19,7 +35,7 @@ safe_levels = 0
 for level in data:
   level = [int(i) for i in level.split(" ")]
   
-  if (not isLevelSafe(level)):
+  if (not is_level_safe(level)):
     continue
   
   safe_levels += 1
